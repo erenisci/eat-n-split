68288e44a5b4c48447d928366f36162586970e32
Eren İşci <isci.eren@gmail.com>
2024-07-13 03:42:41 +0300

    src

--- src/App.js
+++ src/App.js
@@ -73,6 +73,7 @@ const App = () => {
         <FormSplitBill
           selectedFriend={selectedFriend}
           onSplitBill={handleSplitBill}
+          key={selectedFriend.id}
         />
       )}
     </div>
